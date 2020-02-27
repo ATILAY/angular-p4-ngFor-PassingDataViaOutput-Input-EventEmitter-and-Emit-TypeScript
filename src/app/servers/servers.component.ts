2b05924db80685eee89d9666ea0e5b84b9abeaa1
unknown <atilaymayadag@gmail.com>
2020-02-27 17:32:24 +0300

    https://github.com/ATILAY/angular-p4-ngFor-PassingDataViaOutput-Input-EventEmitter-and-Emit-TypeScript

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  serverNameNgModel = '';
+  serverCreationStatusNgModel = 'No server was created!';
+  allowNewServerNgmodel = false;
+
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer =  !this.allowNewServer;
+      this.allowNewServerNgmodel = !this.allowNewServerNgmodel;
+      
+    }, 3000);
+    
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
+  }
+  onUpdateServerName( event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onCreateServerNgModel(){
+    this.serverCreationStatusNgModel = 'Server was created!'+` `+this.serverNameNgModel;
+  }
+  onUpdateServerNameNgModel( event: Event){
+     this.serverNameNgModel = (<HTMLInputElement>event.target).value;
+
+
+
+  }
+
+}
